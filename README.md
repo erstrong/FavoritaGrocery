6b9dc5be74920afa9b7f7059e7da6b73299e6329
Emily Strong <erstrong.06@gmail.com>
2017-12-04 13:43:52 -0500

    Train data cleanse second attempt

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Corporación Favorita Grocery Sales Forecasting
 By Emily Strong and Raksha Kaverappa
 
-## Proposal
+## About This Project
 An overview of our project proposal: [video](https://www.dropbox.com/s/6yrlvrwqdf21cor/Team4ProjectProposal.mp4?dl=0)
+
+(ADD DESCRIPTION)
 
 ## Data Set
 We are using data from the Corporación Favorita [Kaggle competition](https://www.kaggle.com/c/favorita-grocery-sales-forecasting)
+
+## Links
+Live Application: (ADD APP LINK)
+
+Presentation: (ADD SLIDESHARE LINK)
+
+## Test Cases
+
 
--- cleantraindata.py
+++ cleantraindata.py
@@ -1,95 +1,62 @@
 import pandas as pd
 import numpy as np
+import datetime
 
 # Read Data Files
 transactions = pd.read_csv('transactions_clean.csv')
-
+stores = pd.read_csv('stores.csv', header=0)
+transactions = pd.read_csv('transactions_clean.csv', header=0)
+items = pd.read_csv('items.csv', header=0)
+holidays = pd.read_csv('holidays_events.csv', header=0)
+oil = pd.read_csv('oils_clean.csv', header=0)
 
 # Get list of item numbers
 itemlist = pd.read_csv('itemlist.csv', header=None)
-output = pd.DataFrame(transactions['date'])
-output['date'] = pd.to_datetime(output['date'])
-transactions.set_index('date', inplace=True)
-#print(output)
+
+
+# Preprocessing
+holidays = holidays.rename(columns={'date':'date','type':'holidaytype','locale':'locale','locale_name':'locale_name',
+                           'description':'description','transferred':'transferred'})
+nationalholidays = holidays[holidays['locale']=='National']
+regionalholidays = holidays[holidays['locale']=='Regional']
+localholidays = holidays[holidays['locale']=='Local']
+
+localholidays = localholidays.rename(columns={'date':'date','holidaytype':'holidaytype','locale':'locale','locale_name':'city','description':'description','transferred':'transferred'})
+regionalholidays = regionalholidays.rename(columns={'date':'date','holidaytype':'holidaytype','locale':'locale','locale_name':'state','description':'description','transferred':'transferred'})
+
+
+transactions = transactions.melt(id_vars=['date'])
+transactions = transactions.rename(columns={'date':'date','variable':'store_nbr','value':'transactions'})
+transactions['store_nbr'] = transactions['store_nbr'].map(lambda x: int(x))
+
+
 
 def clean(item):
     print(str(item))
     filename= 'items/'+ str(item) + '.csv'
     df = pd.read_csv(filename)
-    df = df.pivot_table(index='date', columns=['store_nbr'], values=['unit_sales'])
-    flattened = pd.DataFrame(df.to_records())
-    flattened.columns = [hdr.replace("('unit_sales', ","").replace(")", "") for hdr in flattened.columns]
-    flattened['date'] =  pd.to_datetime(flattened['date'], format='%Y/%m/%d')
-    flattened.set_index('date', inplace=True)
-    flattened = flattened.resample('D').mean().reset_index()
-    flattened.set_index('date', inplace=True)
-    stores = flattened.columns.tolist()
-    temp_transactions = transactions[stores]
-    for store in stores:
-        if (store==20):
-            t20 = flattened['20']
-            t20 = t20.loc['2015-02-13':]
-            t20['20'] = t20['20'].interpolate(method='linear').bfill().ffill()
-            flattened['20'] = t20['20']
-        elif (store==21):
-            t21 = flattened['21']
-            t21 = t21.loc['2015-07-24':]
-            t21['21'] = t21['21'].interpolate(method='linear').bfill().ffill()
-            flattened['21'] = t21['21']
-        elif (store==22):
-            t22 = flattened['22']
-            t22 = t22.loc['2015-10-09':]
-            t22['22'] = t22['22'].interpolate(method='linear').bfill().ffill()
-            flattened['22'] = t22['22']
-        elif (store==29):
-            t29 = flattened['29']
-            t29 = t29.loc['2015-03-20':]
-            t29['29'] = t29['29'].interpolate(method='linear').bfill().ffill()
-            flattened['29'] = t29['29']
-        elif (store==36):
-            t36 = flattened['36']
-            t36 = t36.loc['2013-05-10':]
-            t36['36'] = t36['36'].interpolate(method='linear').bfill().ffill()
-            flattened['36'] = t36['36']
-        elif (store==42):
-            t42 = flattened['42']
-            t42 = t42.loc['2015-08-21':]
-            t42['42'] = t42['42'].interpolate(method='linear').bfill().ffill()
-            flattened['42'] = t42['42']
-        elif (store==52):
-            t52 = flattened['52']
-            t52 = t52.loc['2017-04-20':]
-            t52['52'] = t52['52'].interpolate(method='linear').bfill().ffill()
-            flattened['52'] = t52['52']
-        elif (store==53):
-            t53 = flattened['53']
-            t53 = t53.loc['2014-05-29':]
-            t53['53'] = t53['53'].interpolate(method='linear').bfill().ffill()
-            flattened['53'] = t53['53']
-        else:
-            flattened[store] = flattened[store].interpolate(method='linear').bfill().ffill()
-        flattened[store] = flattened[store]/transactions[store]
-    flattened[item] = flattened.mean(axis=1)
-    flattened['date'] = pd.to_datetime(flattened.index.values)
-    columns = ['date', item]
-    return flattened[columns]
+    df['onpromotion'].fillna(False, inplace=True)
+    # Combine with other files
+    df = df.merge(items, on='item_nbr', how='left')
+    df = df.merge(stores, on='store_nbr', how='left')
+    #df = df['store_nbr'].map(lambda a: int(a))
+    df = df.merge(transactions, how='left', on=['date','store_nbr'])
+    df = df.merge(oil, how='left', on='date')
+    df = df.merge(nationalholidays, on='date', how='left')
+    df = df.merge(regionalholidays, on=['date','state'], how='left', suffixes=('_n','_r'))
+    df = df.merge(localholidays, on=['date','city'], how='left')
+    # Convert date to datetime
+    format = '%Y-%m-%d'
+    df['date'] = df['date'].map(lambda a: datetime.datetime.strptime(a, format))
+    df['day'] = df['date'].map(lambda x: x.weekday())
+    df['month'] = df['date'].map(lambda x: x.month)
+    df['year'] = df['date'].map(lambda x: x.year)
+    df.to_csv('mergeitems/'+ str(item) + '.csv', index=False)
 
 
-df_collection=[]
 
 for index, row in itemlist.iterrows():
     item = int(row[0])
-    df_collection.append(clean(item))
+    clean(item)
 
-for df in df_collection:
-    df['date'] = pd.to_datetime(df['date'])
-    output = output.merge(df, on='date', how='left')
-
-#for index, row in itemlist.iterrows():
-    #item = int(row[0])
-    #output[item] = output[item].interpolate(method='linear').bfill().ffill()
-
-output.fillna(0, inplace=True)
-#output.to_csv('train_finalclean.csv')
-output.to_csv('train_zeros.csv')
 print('Success!')
