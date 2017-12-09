448feeccb35ea775d3948c8c80f3826e902dabab
Emily Strong <erstrong.06@gmail.com>
2017-12-09 15:36:18 -0500

    directory cleanup

--- Part3-ClusteringandModels/Clustering/trainclustering.py
+++ Part3-ClusteringandModels/Clustering/trainclustering.py
@@ -3,18 +3,9 @@ from sklearn.cluster import KMeans
 import pickle
 from sklearn.externals import joblib
 
-data = pd.read_csv('output.csv')
+data = pd.read_csv('train_clean.csv')
 
-data.columns=['id','','','','','','','','']
-
-data.drop('id', axis=1, inplace=True)
-data.drop('store_nbr', axis=1, inplace=True)
-data.drop('item_nbr', axis=1, inplace=True)
-data.drop('holidaytype', axis=1, inplace=True)
-data.drop('holidaytype_n', axis=1, inplace=True)
-data.drop('holidaytype_r', axis=1, inplace=True)
-
-kmeanModel = KMeans(n_clusters=5)
+kmeanModel = KMeans(n_clusters=10)
 kmeanModel.fit(data)
 
 data['label'] = kmeanModel.labels_
@@ -24,9 +15,22 @@ data1 = data[data['label']==1]
 data2 = data[data['label']==2]
 data3 = data[data['label']==3]
 data4 = data[data['label']==4]
+data5 = data[data['label']==5]
+data6 = data[data['label']==6]
+data7 = data[data['label']==7]
+data8 = data[data['label']==8]
+data9 = data[data['label']==9]
+
 
 data0.to_csv('train_cluster0.csv', index=False)
 data1.to_csv('train_cluster1.csv', index=False)
 data2.to_csv('train_cluster2.csv', index=False)
 data3.to_csv('train_cluster3.csv', index=False)
 data4.to_csv('train_cluster4.csv', index=False)
+data5.to_csv('train_cluster5.csv', index=False)
+data6.to_csv('train_cluster6.csv', index=False)
+data7.to_csv('train_cluster7.csv', index=False)
+data8.to_csv('train_cluster8.csv', index=False)
+data9.to_csv('train_cluster9.csv', index=False)
+
+Print('Success!')
