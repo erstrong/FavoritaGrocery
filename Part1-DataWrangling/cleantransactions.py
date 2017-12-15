ace3e3550257145a1e7de9018675c0f620a7a165
Emily Strong <erstrong.06@gmail.com>
2017-12-15 17:24:16 -0500

    final code commit

---
+++ Part1-DataWrangling/cleantransactions.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from datetime import datetime
+
+# Read data file
+df = pd.read_csv('transactions.csv', low_memory=False)
+
+# Convert to pivot table
+df2 = df.pivot_table(index='date', columns=['store_nbr'], values=['transactions'])
+flattened = pd.DataFrame(df2.to_records())
+flattened.columns = [hdr.replace("('transactions', ","").replace(")", "") for hdr in flattened.columns]
+
+# Fill in missing dates
+flattened['date'] =  pd.to_datetime(flattened['date'], format='%Y/%m/%d')
+
+flattened.set_index('date', inplace=True)
+
+flattened = flattened.resample('D').reset_index()
+
+
+# Interpolate missing values for stores that existed on 1/2/13
+for i in range(1, 20):
+    flattened[str(i)] = flattened[str(i)].interpolate(method='linear').bfill().ffill()
+
+for i in range(23, 29):
+    flattened[str(i)] = flattened[str(i)].interpolate(method='linear').bfill().ffill()
+
+for i in range(30, 36):
+    flattened[str(i)] = flattened[str(i)].interpolate(method='linear').bfill().ffill()
+
+for i in range(37, 42):
+    flattened[str(i)] = flattened[str(i)].interpolate(method='linear').bfill().ffill()
+
+for i in range(43, 52):
+    flattened[str(i)] = flattened[str(i)].interpolate(method='linear').bfill().ffill()
+
+flattened['54'] = flattened['54'].interpolate(method='linear').bfill().ffill()
+
+# Interpolate missing values for stores opened after 1/2/13
+#20 2015-02
+t20 = flattened[['date','20']]
+t20 = t20[t20['date']>='2015-02-13']
+t20['20'] = t20['20'].interpolate(method='linear').bfill().ffill()
+flattened['20'] = t20['20']
+
+#21 2015-07
+t21 = flattened[['date','21']]
+t21 = t21[t21['date']>='2015-07-24']
+t21['21'] = t21['21'].interpolate(method='linear').bfill().ffill()
+flattened['21'] = t21['21']
+
+
+#22 2015-10
+t22 = flattened[['date','22']]
+t22 = t22[t22['date']>='2015-10-09']
+t22['22'] = t22['22'].interpolate(method='linear').bfill().ffill()
+flattened['22'] = t22['22']
+
+
+#29 2015-03
+t29 = flattened[['date','29']]
+t29 = t29[t29['date']>='2015-03-20']
+t29['29'] = t29['29'].interpolate(method='linear').bfill().ffill()
+flattened['29'] = t29['29']
+
+
+#36 2013-05
+t36 = flattened[['date','36']]
+t36 = t36[t36['date']>='2013-05-09']
+t36['36'] = t36['36'].interpolate(method='linear').bfill().ffill()
+flattened['36'] = t36['36']
+
+
+#42 2015-08
+t42 = flattened[['date','42']]
+t42 = t42[t42['date']>='2015-08-21']
+t42['42'] = t42['42'].interpolate(method='linear').bfill().ffill()
+flattened['42'] = t42['42']
+
+
+#52 2017-04
+t52 = flattened[['date','52']]
+t52 = t52[t52['date']>='2017-04-20']
+t52['52'] = t52['52'].interpolate(method='linear').bfill().ffill()
+flattened['52'] = t52['52']
+
+
+#53 2014-05
+t53 = flattened[['date','53']]
+t53 = t53[t53['date']>='2014-05-29']
+t53['53'] = t53['53'].interpolate(method='linear').bfill().ffill()
+flattened['53'] = t53['53']
+
+flattened.to_csv('transactions_clean.csv', index=False)
+print('Success!')
--- README.md
+++ README.md
@@ -5,12 +5,15 @@ Final Project for INFO 7390: Advances in Data Science and Architecture
 ## Authors
 Emily Strong and Raksha Kaverappa
 
+## About
+This is a project to forecast grocery sales for Corporación Favorita, an Ecuadorian grocery chain. The data comes from an active Kaggle competition which is why we have not posted it here.
+
 ## Live App
 [https://groceryforecasting.herokuapp.com/](https://groceryforecasting.herokuapp.com/)
 
-Note: Because we used Heroku to deploy our web app, we had to create a separate repository for the app code
+Note: Because we used Heroku to deploy our web app, we had to create a separate repository for the app code. We have copied the live code to the Part5-Copy folder, but you can see the separate repository here:
 
 App Repository: [https://github.com/erstrong/GroceryFlaskApp](https://github.com/erstrong/GroceryFlaskApp)
 
 ## References
-App Template: [Flask Boilerplate](https://github.com/mjhea0/flask-boilerplate)
+Kaggle Competition:[https://www.kaggle.com/c/favorita-grocery-sales-forecasting](https://www.kaggle.com/c/favorita-grocery-sales-forecasting)
